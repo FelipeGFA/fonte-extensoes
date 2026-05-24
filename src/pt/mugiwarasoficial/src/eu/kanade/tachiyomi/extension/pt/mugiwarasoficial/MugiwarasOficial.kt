225a909c743319da84d6c63753142e7374456416
Felipe Avila <felipegabriel.avila6@gmail.com>
2026-05-24 02:04:36 -0300

    Mugiwaras: fix pages

--- src/pt/mugiwarasoficial/src/eu/kanade/tachiyomi/extension/pt/mugiwarasoficial/MugiwarasOficial.kt
+++ src/pt/mugiwarasoficial/src/eu/kanade/tachiyomi/extension/pt/mugiwarasoficial/MugiwarasOficial.kt
@@ -1,12 +1,7 @@
 package eu.kanade.tachiyomi.extension.pt.mugiwarasoficial
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
-import eu.kanade.tachiyomi.source.model.Page
-import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.HttpUrl.Companion.toHttpUrl
-import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -26,17 +21,5 @@ class MugiwarasOficial :
 
     override val useLoadMoreRequest = LoadMoreStrategy.Never
 
-    override fun pageListParse(document: Document): List<Page> {
-        val redirectUrl = document.selectFirst("div.page-break a")!!.absUrl("href")
-        val pageUrl = redirectUrl.toHttpUrl().queryParameter("t")!!.toHttpUrl().toUrl()
-
-        val url = "$baseUrl/campanha.php".toHttpUrl().newBuilder()
-            .addQueryParameter("auth", pageUrl.toString())
-            .build()
-
-        return client.newCall(GET(url, headers)).execute().asJsoup()
-            .select(".manga-content img").mapIndexed { index, elemet ->
-                Page(index, imageUrl = elemet.absUrl("src"))
-            }
-    }
+    override val pageListParseSelector = "img.wp-manga-chapter-img"
 }
