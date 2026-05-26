acc06dc56be9a3bebfda89bbae53e2ba24d0690d
FelipeGFA <felipegabriel.avila6@gmail.com>
2026-05-26 20:30:07 -0300

    fix hentaitokyo

--- src/pt/hentaitokyo/src/eu/kanade/tachiyomi/extension/pt/hentaitokyo/HentaiTokyo.kt
+++ src/pt/hentaitokyo/src/eu/kanade/tachiyomi/extension/pt/hentaitokyo/HentaiTokyo.kt
@@ -2,7 +2,10 @@ package eu.kanade.tachiyomi.extension.pt.hentaitokyo
 
 import eu.kanade.tachiyomi.multisrc.gattsu.Gattsu
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import java.util.concurrent.TimeUnit
 
 class HentaiTokyo :
@@ -15,4 +18,23 @@ class HentaiTokyo :
     override val client: OkHttpClient = super.client.newBuilder()
         .rateLimit(1, 2, TimeUnit.SECONDS)
         .build()
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        val document = response.asJsoup()
+
+        return listOf(
+            SChapter.create().apply {
+                name = "Capítulo único"
+                scanlator = document.selectFirst("ul.post-itens li:contains(Tradutor) a")?.text()
+                date_upload = document.selectFirst("meta[property=article:published_time]")
+                    ?.attr("content")
+                    .orEmpty()
+                    .toDate()
+                setUrlWithoutDomain(response.request.url.toString())
+            },
+        )
+    }
+
+    override fun pageListSelector(): String = "div.meio ul.post-fotos img, " +
+        "div.meio div.post-box.listaImagens div.galeriaHtml img"
 }
